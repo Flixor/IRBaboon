b00567d70870f4c2c5034d15a6ff0960e7a5f5cc
Felix <felixhopostma@gmail.com>
2020-10-21 17:41:21 +0200

    managed to change executable name to IRBaboon
    and not IRBaboonCombined. Also now says 'Flixor' rather than 'yourcompany'

--- IRBaboonCombined/JuceLibraryCode/JucePluginDefines.h
+++ IRBaboonCombined/JuceLibraryCode/JucePluginDefines.h
@@ -38,13 +38,13 @@
  #define JucePlugin_Enable_IAA             0
 #endif
 #ifndef  JucePlugin_Name
- #define JucePlugin_Name                   "IRBaboonCombined"
+ #define JucePlugin_Name                   "IRBaboon"
 #endif
 #ifndef  JucePlugin_Desc
- #define JucePlugin_Desc                   "IRBaboonCombined"
+ #define JucePlugin_Desc                   "IRBaboon"
 #endif
 #ifndef  JucePlugin_Manufacturer
- #define JucePlugin_Manufacturer           "yourcompany"
+ #define JucePlugin_Manufacturer           "Flixor"
 #endif
 #ifndef  JucePlugin_ManufacturerWebsite
  #define JucePlugin_ManufacturerWebsite    ""
@@ -98,16 +98,16 @@
  #define JucePlugin_AUSubType              JucePlugin_PluginCode
 #endif
 #ifndef  JucePlugin_AUExportPrefix
- #define JucePlugin_AUExportPrefix         IRBaboonCombinedAU
+ #define JucePlugin_AUExportPrefix         IRBaboonAU
 #endif
 #ifndef  JucePlugin_AUExportPrefixQuoted
- #define JucePlugin_AUExportPrefixQuoted   "IRBaboonCombinedAU"
+ #define JucePlugin_AUExportPrefixQuoted   "IRBaboonAU"
 #endif
 #ifndef  JucePlugin_AUManufacturerCode
  #define JucePlugin_AUManufacturerCode     JucePlugin_ManufacturerCode
 #endif
 #ifndef  JucePlugin_CFBundleIdentifier
- #define JucePlugin_CFBundleIdentifier     com.yourcompany.IRBaboonCombined
+ #define JucePlugin_CFBundleIdentifier     com.Flixor.IRBaboon
 #endif
 #ifndef  JucePlugin_RTASCategory
  #define JucePlugin_RTASCategory           0
@@ -125,7 +125,7 @@
  #define JucePlugin_RTASDisableMultiMono   0
 #endif
 #ifndef  JucePlugin_AAXIdentifier
- #define JucePlugin_AAXIdentifier          com.yourcompany.IRBaboonCombined
+ #define JucePlugin_AAXIdentifier          com.Flixor.IRBaboon
 #endif
 #ifndef  JucePlugin_AAXManufacturerCode
  #define JucePlugin_AAXManufacturerCode    JucePlugin_ManufacturerCode
@@ -149,7 +149,7 @@
  #define JucePlugin_IAASubType             JucePlugin_PluginCode
 #endif
 #ifndef  JucePlugin_IAAName
- #define JucePlugin_IAAName                "yourcompany: IRBaboonCombined"
+ #define JucePlugin_IAAName                "Flixor: IRBaboon"
 #endif
 #ifndef  JucePlugin_VSTNumMidiInputs
  #define JucePlugin_VSTNumMidiInputs       16
