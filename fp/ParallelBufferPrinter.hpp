7596517e03320b051390d05643c84298aa62ffe6
Felix <felixhopostma@gmail.com>
2021-02-08 17:36:35 -0300

    added description, and moved PrintBuffer to sensible scope
    PrintBuffer is now a class within ParallelPrintBuffer.

--- fp/ParallelBufferPrinter.hpp
+++ fp/ParallelBufferPrinter.hpp
@@ -1,10 +1,8 @@
-//
-//  Copyright © 2020 Felix Postma. 
-//
-
+/*
+ *  Copyright © 2021 Felix Postma. 
+ */
 
 #pragma once
-
 
 #include <fp_include_all.hpp>
 #include <JuceHeader.h>
@@ -13,26 +11,31 @@
 namespace fp {
 
 
-class PrintBuffer {
-    
-public:
-    PrintBuffer();
-	PrintBuffer(std::string h, AudioBuffer<float>& b);
-    ~PrintBuffer();
-    
-    std::string header;
-    AudioBuffer<float> buffer;
-    bool empty;
-};
-
-
+/* The ParallelBufferPrinter provides a way to debug AudioSampleBuffers in XCode.
+ * During runtime buffers can be added to the printer, and then one of the print functions
+ * can be used to check the contents at the time of adding. 
+ */	
 
 class ParallelBufferPrinter{
     
 public:
+
+	class PrintBuffer {
+	    
+	public:
+	    PrintBuffer();
+		PrintBuffer(std::string h, AudioBuffer<float>& b);
+	    ~PrintBuffer();
+	    
+	    std::string header;
+	    AudioBuffer<float> buffer;
+	    bool empty;
+	};
+
     ParallelBufferPrinter();
     ~ParallelBufferPrinter();
     
+
 	void appendBuffer(std::string name, AudioBuffer<float>& buffer);
 	void appendCircularBufferArray(std::string name, fp::CircularBufferArray& circularBufferArray);
 
@@ -40,9 +43,8 @@ public:
     void insertBuffer(std::string name, AudioBuffer<float>& buffer, int insertIndex);
 	
 	void printToConsole(int startSample, int numSamples, bool onlyCh0 = false, bool displaydB = false);
-	
-	void printToWav(int startSample, int numSamples, int sampleRate, String directoryNameFullPath = "-");
 
+	void printToWav(int startSample, int numSamples, int sampleRate, String directoryNameFullPath = "-");
 
 	// assumes that all added buffers have the same samplerate
 	// don't forget to include libboost_filesystem.dylib in your project!
