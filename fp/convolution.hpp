f235e7401974caff05fdaca1da1f80d974fc31d1
Felix <felixhopostma@gmail.com>
2021-02-17 17:33:13 -0300

    Revert "changed ptr arg to ref arg, and changed indent levels"
    This reverts commit 2be22ac066c1776b26a328426f6a437f70ef17e9.

--- fp/convolution.hpp
+++ fp/convolution.hpp
@@ -15,27 +15,27 @@ namespace fp {
  * and helpers only used by convolution.
  */	
 
-enum ChannelLayout{
-	unknown,
-	IRMonoAudioMono,
-	IRMonoAudioStereo,
-	IRStereoAudioMono,
-	IRStereoAudioStereo
-};
-
-namespace convolution {
+	enum ChannelLayout{
+		unknown,
+		IRMonoAudioMono,
+		IRMonoAudioStereo,
+		IRStereoAudioMono,
+		IRStereoAudioStereo
+	};
 	
-	
-	/* convolve() uses complex multiplication
-	 * buffer1 is seen as "input audio" and buffer2 as the "IR" to convolve with */
-	AudioBuffer<float> convolvePeriodic(AudioBuffer<float>& buffer1, AudioBuffer<float>& buffer2, int processBlockSize = 256);
-	AudioBuffer<float> convolveNonPeriodic(AudioBuffer<float>& buffer1, AudioBuffer<float>& buffer2);
-	
-	/* Deconvolution is non-periodic, meaning it won't fold back, 
-	 * but outputs a buffer that is at least N_numerator + N_denominator + 1 samples.
-	 * The deconvolution uses complex division.
-	 * The denominator buffer needs to be mono! */
-	AudioBuffer<float> deconvolve(AudioBuffer<float>& numeratorBuffer, AudioBuffer<float>& denominatorBuffer, double sampleRate, bool smoothing = true, bool includePhase = true, bool includeAmplitude = true);
+	namespace convolution {
+		
+		
+		/* convolve() uses complex multiplication
+		 * buffer1 is seen as "input audio" and buffer2 as the "IR" to convolve with */
+		AudioBuffer<float> convolvePeriodic(AudioBuffer<float>& buffer1, AudioBuffer<float>& buffer2, int processBlockSize = 256);
+		AudioBuffer<float> convolveNonPeriodic(AudioBuffer<float>& buffer1, AudioBuffer<float>& buffer2);
+		
+		/* Deconvolution is non-periodic, meaning it won't fold back, 
+		 * but outputs a buffer that is at least N_numerator + N_denominator + 1 samples.
+		 * The deconvolution uses complex division.
+		 * The denominator buffer needs to be mono! */
+		AudioBuffer<float> deconvolve(AudioBuffer<float>* numeratorBuffer, AudioBuffer<float>* denominatorBuffer, double sampleRate, bool smoothing = true, bool includePhase = true, bool includeAmplitude = true);
 
 	/* averagingFilter has a low-pass effect in the upper freqs if the fft method performForwardRealFreqOnly() has been used,
 	 * because the upper bin range will eventually go into negative freq bin territory, where the contents of the bins are 0,
@@ -43,6 +43,6 @@ namespace convolution {
 	 * negative logAvg = linear average instead. */
 	void averagingFilter (AudioBuffer<float>* buffer, double octaveFraction, double sampleRate, bool logAvg, bool nullifyPhase = false, bool nullifyAmplitude = false);
 
-} // convolution
+	} // convolution
 	
 } // fp
